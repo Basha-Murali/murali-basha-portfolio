ba6d9c9185eb0404a3c7da04363202f032442a16
Murali-Basha <Mbasha@trcsolutions.com>
2025-10-17 18:20:16 +0530

    fix: vite base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
+  root: ".", // 👈 tells Vite to treat ./ as root (what Vercel uses)
   plugins: [react()],
-  base: "./", // 👈 ensures relative asset paths in build
+  base: "./", // 👈 ensures relative paths in production
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: resolve(__dirname, "index.html"), // 👈 absolute path fix for Vercel
+    },
+  },
 });
